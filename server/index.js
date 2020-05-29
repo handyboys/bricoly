b8d0a08b622f358bb8cf61fe83d94707b1f05acf
raffa <raffa.rayen@gmail.com>
2020-05-29 19:57:03 +0100

    initialize server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server listening on port : ${port} ..`);
+});
