54282d79e48f1242c265696ab019f7860a1ce5c7
raffa <raffa.rayen@gmail.com>
2020-05-29 11:30:53 +0100

    init ui structure

--- bricoly-ui/src/app/app.module.ts
+++ bricoly-ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { ToggleFilterDirective } from './directives/toggle-filter.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SigninComponent,
+    ToggleFilterDirective
   ],
   imports: [
     BrowserModule,
