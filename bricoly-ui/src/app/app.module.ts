f69f7235355d62b981f13c47dbde2289af995abe
MootezDj <djebalimootez1@gmail.com>
2020-06-08 09:26:05 +0300

    working on the map

--- bricoly-ui/src/app/app.module.ts
+++ bricoly-ui/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule,  HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // Bootstrap-based material design UI library
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule } from 'angular-bootstrap-md'; 
 
+import { AgmCoreModule } from '@agm/core';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToggleFilterDirective } from './directives/toggle-filter.directive';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
 
 
 
@@ -23,13 +25,17 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AppComponent,
     ToggleFilterDirective,
     NavbarComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB3Z8u3uZ-qxEQW9xusAZIRnpUULTBI1X8'
+    })
   ],
   providers: [
     {
