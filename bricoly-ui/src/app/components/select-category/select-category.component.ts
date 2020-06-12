c7ad1138e2b14ea6e2fadaa99c1b280acab56125
raffa <raffa.rayen@gmail.com>
2020-06-12 18:01:06 +0100

    refactor navigation to next step in job post sequence

--- bricoly-ui/src/app/components/select-category/select-category.component.ts
+++ bricoly-ui/src/app/components/select-category/select-category.component.ts
@@ -1,7 +1,7 @@
 import { Category } from './../../models/category/category.model';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { JobPostService } from '../../services/jobPost/job-post.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 //import {from} from 'rxjs';
@@ -16,7 +16,7 @@ export class SelectCategoryComponent implements OnInit {
   
   @Output () selectCategoryEvent = new EventEmitter<object>();
   
-  constructor( private jobPost:JobPostService, private route: ActivatedRoute ) {
+  constructor( private jobPost:JobPostService, private route: ActivatedRoute, private router: Router ) {
   console.log('sel-cat constructor : ', this.route.snapshot.data)
    this.selectCategories = this.route.snapshot.data.categories;
   }
@@ -28,6 +28,7 @@ export class SelectCategoryComponent implements OnInit {
   sendCategory(selectedCategory){
     console.log('selectcategory com : ',selectedCategory)
     this.selectCategoryEvent.emit(selectedCategory)
+    this.router.navigate(['../select-service'], { relativeTo: this.route });
   }
  
 
